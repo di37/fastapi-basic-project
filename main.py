8d5b75659e7562c1a61dbe1d0ccd2ca360d726a4
isham993 <d.isham.993@gmail.com>
2022-11-06 20:14:22 +0400

    CORS included.

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ from models.models import (
     Product,
 )
 
+## Adding cors headers
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Adding CORS urls and add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
